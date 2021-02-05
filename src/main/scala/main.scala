05785d2ef4a415a1476135a8deb5660761a535e2
Gabriel Jeantet <gabriel.jeantetpro@gmail.com>
2021-02-05 09:21:26 +0100

    Nico débute

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,72 +1,122 @@
 import swing._
 import swing.event._
+import java.awt.{Color,Graphics2D,BasicStroke, Graphics}
+import java.awt.geom._
+import java.awt.image.BufferedImage                                           
+import java.io.File                                                           
+import javax.imageio.ImageIO
 
-object My_app extends SimpleSwingApplication 
-{
+
+object ColorChanger {
+
+  def copyImage(source: BufferedImage): BufferedImage = {
+    val b:BufferedImage = new BufferedImage(source.getWidth(), source.getHeight(), source.getType());
+    val g:Graphics = b.getGraphics();
+    g.drawImage(source, 0, 0, null);
+    g.dispose();
+    return b;
+  }
+
+
+  def changeColor(original: BufferedImage, oldRed: Int, oldGreen: Int , oldBlue: Int, newRed: Int, newGreen: Int , newBlue: Int): BufferedImage = {
+    val RGB_MASK:Int = 0x00FFFFFF
+    val ALPHA_MASK:Int = 0xFF000000
+    val oldRGB:Int = oldRed << 16 | oldGreen << 8 | oldBlue
+    val toggleRGB = oldRGB ^ (newRed << 16 | newGreen << 8 | newBlue)
+    val w:Int = original.getWidth()
+    val h:Int = original.getHeight()
+
+    val newImage: BufferedImage = copyImage(original)
+
+    var rgb = newImage.getRGB(0,0,w,h,null,0,w)
+    for( i <- 0 to (rgb.length-1)){
+      if( (rgb(i) & RGB_MASK) == oldRGB){
+        rgb(i) ^= toggleRGB
+      }
+    }
+    
+    newImage.setRGB(0,0,w,h,rgb,0,w)
+
+    return newImage
+  }
+}
+
+
+
+class GamePanel(main_frame: MainFrame ) extends Panel {
   var width = 1280
   var height = 720
 
+  val matrix_width = 160
+  val matrix_height = 90
+
+  var px = 50
+  var py = 40
+
+  this.preferredSize = new Dimension(width, height)
+
+  private var tilesetWhite:BufferedImage = null 
+  tilesetWhite = ImageIO.read(new File("src/main/resources/2.png"))
+  val tileSets = scala.collection.mutable.Map[String,BufferedImage]()
+  tileSets("255255255") = tilesetWhite
+  def getColoredTileset(color:String):BufferedImage = {
+    val red = (color.substring(0,3)).toInt
+    val green = (color.substring(3,6)).toInt
+    val blue = (color.substring(6,9)).toInt
+    println(red + "  " + green + "  " + blue + "\n")
+    if( tileSets.contains(color) ){
+      return tileSets(color)
+    }else{
+      tileSets(color) = ColorChanger.changeColor(tilesetWhite, 255,255,255,red,green,blue)
+      return tileSets(color)
+    }
+  }
+
+  def paintCharacter(g: Graphics2D,c: Int, px: Int, py: Int, bg: Color, fg: String){
+    val x = c % 16 
+    val y = c / 16 
+    val dx1 = x*12 
+    val dy1 = y*12
+    val dx2 = dx1+11
+    val dy2 = dy1+11
+    val sx1 = px
+    val sy1 = py
+    val sx2 = px + 11
+    val sy2 = py + 11
+    g.drawImage(getColoredTileset(fg), sx1, sy1, sx2, sy2, dx1, dy1, dx2, dy2, bg , null)
+  }
+  
+  override def paintComponent(g : Graphics2D) {
+    g.setRenderingHint(java.awt.RenderingHints.KEY_ANTIALIASING, java.awt.RenderingHints.VALUE_ANTIALIAS_ON)
+    
+    g.setColor(Color.BLACK);
+    g.fillRect(0,0, width, height)
+
+    if (null != tilesetWhite)
+      //g.setColor(Color.BLUE);
+      //g.fillRect(px,py, 192, 192)
+    paintCharacter(g, 1, px, py, Color.BLACK, "222093013")
+      //g.drawImage(tilesetRed, px, py, null)
+  }
+
+  focusable = true
+  listenTo(mouse.clicks)
+  listenTo(keys)
+  //**************************************************************//
+  //************* reactions correspond à la MainLoop *************//
+  //**************************************************************//
+  this.reactions += {
+    case KeyPressed(_,k,_,_) => println(k+"\n")
+    case MouseClicked(_,_,_,_,_) => {}
+  }
+}
+
+object My_app extends SimpleSwingApplication 
+{
   def top = new MainFrame { main_frame =>
-    title = "Launcher"
+    title = "Enter the crapgeon!"
     resizable = false
-    /********* Launcher ***************************************/
-    /**********************************************************/
-    val launch_button = new Button {
-      text = "Launch game"
-    }
-    val launch_label = new Label {
-      text = "Please choose your resolution's width"
-    }
-    val launch_width_field = new TextField{
-      text = "1280"
-    }
-    val launch_width_label = new Label{
-      text = "width = "
-    }
-    val launch_height_label = new Label {
-      text = "height = 720"
-    }
-    val launcher_box = new BoxPanel(Orientation.Vertical) {
-      launcher_box => 
-      contents += launch_label
-      contents += new BoxPanel(Orientation.Horizontal) {
-        contents += launch_width_label
-        contents += launch_width_field
-      }
-      contents += launch_height_label
-      contents += launch_button
-      border = Swing.EmptyBorder(60, 80, 60, 60)
-      listenTo(launch_button, launch_width_field)
-      reactions += {
-        case EditDone(`launch_width_field`) =>
-          launch_width_field.text match {
-            case "" => launch_height_label.text = "height = "
-            case _ => 
-              val f = (launch_width_field.text.toInt)/16*9
-              launch_height_label.text = "height = " + f.toString
-          }
-        case ButtonClicked(launch_button) =>
-          launch_width_field.text match {
-            case "" => {}
-            case _ => 
-              val game_width = launch_width_field.text.toInt
-              val game_height = game_width/16*9
-              game_box.border = Swing.EmptyBorder(game_height, game_width, 0, 0) 
-              main_frame.contents = game_box
-              main_frame.title = "Enter the crapgeon!"
-          }
-      }
-    }
-    contents = launcher_box
-    /********* MainGame ***************************************/
-    /**********************************************************/
-    val canvas = new Label{
-      foreground = java.awt.Color.black
-    }
-    val game_box = new BoxPanel(Orientation.Vertical) {
-      launcher_box =>
-        border = Swing.EmptyBorder(0,0,0,0) 
-        contents += canvas
-    }
+    visible = true
+    contents = new GamePanel(this)
   }
 }

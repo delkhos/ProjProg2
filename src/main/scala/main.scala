f76adb8f2bebcb341a98bd9d645b081fe578722b
Gabriel Jeantet <gabriel.jeantetpro@gmail.com>
2021-02-05 10:57:47 +0100

    gitu commit

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -41,29 +41,42 @@ object ColorChanger {
   }
 }
 
-
-
 class GamePanel(main_frame: MainFrame ) extends Panel {
   var width = 1280
   var height = 720
 
-  val matrix_width = 160
-  val matrix_height = 90
+  val matrix_width = 80
+  val matrix_height = 45
 
-  var px = 50
-  var py = 40
+  var px = 1
+  var py = 1
+
+  var tileset_size = 16
+  var game_colors = Array[String]("255255255","255000000","000255000","000000255")
+  var current_color = 0
 
   this.preferredSize = new Dimension(width, height)
 
   private var tilesetWhite:BufferedImage = null 
-  tilesetWhite = ImageIO.read(new File("src/main/resources/2.png"))
+  tilesetWhite = ImageIO.read(new File("src/main/resources/3_16.png"))
   val tileSets = scala.collection.mutable.Map[String,BufferedImage]()
   tileSets("255255255") = tilesetWhite
+  for( i <- 1 to  (game_colors.size-1)){
+    getColoredTileset(game_colors(i)) 
+  }
+
+  def toInt(s: String): Int = {
+    try {
+      s.toInt
+    } catch {
+      case e: Exception => 255
+    }
+  }
   def getColoredTileset(color:String):BufferedImage = {
-    val red = (color.substring(0,3)).toInt
-    val green = (color.substring(3,6)).toInt
-    val blue = (color.substring(6,9)).toInt
-    println(red + "  " + green + "  " + blue + "\n")
+    val red = toInt(color.substring(0,3))
+    val green = toInt(color.substring(3,6))
+    val blue = toInt(color.substring(6,9))
+    //println("Debug : " + red + "  " + green + "  " + blue + "\n")
     if( tileSets.contains(color) ){
       return tileSets(color)
     }else{
@@ -75,28 +88,35 @@ class GamePanel(main_frame: MainFrame ) extends Panel {
   def paintCharacter(g: Graphics2D,c: Int, px: Int, py: Int, bg: Color, fg: String){
     val x = c % 16 
     val y = c / 16 
-    val dx1 = x*12 
-    val dy1 = y*12
-    val dx2 = dx1+11
-    val dy2 = dy1+11
-    val sx1 = px
-    val sy1 = py
-    val sx2 = px + 11
-    val sy2 = py + 11
+    val dx1 = x*tileset_size 
+    val dy1 = y*tileset_size
+    val dx2 = dx1+(tileset_size-1)
+    val dy2 = dy1+(tileset_size-1)
+    val size = width/matrix_width
+    val sx1 = (px-1)*size
+    val sy1 = (py-1)*size
+    val sx2 = sx1 + (size-1)
+    val sy2 = sy1 + (size-1)
     g.drawImage(getColoredTileset(fg), sx1, sy1, sx2, sy2, dx1, dy1, dx2, dy2, bg , null)
   }
   
   override def paintComponent(g : Graphics2D) {
-    g.setRenderingHint(java.awt.RenderingHints.KEY_ANTIALIASING, java.awt.RenderingHints.VALUE_ANTIALIAS_ON)
+    g.setRenderingHint(java.awt.RenderingHints.KEY_ANTIALIASING, java.awt.RenderingHints.VALUE_ANTIALIAS_OFF)
+    g.setRenderingHint(java.awt.RenderingHints.KEY_ALPHA_INTERPOLATION, java.awt.RenderingHints.VALUE_ALPHA_INTERPOLATION_SPEED)
+    g.setRenderingHint(java.awt.RenderingHints.KEY_COLOR_RENDERING, java.awt.RenderingHints.VALUE_COLOR_RENDER_SPEED)
     
     g.setColor(Color.BLACK);
     g.fillRect(0,0, width, height)
 
-    if (null != tilesetWhite)
-      //g.setColor(Color.BLUE);
-      //g.fillRect(px,py, 192, 192)
-    paintCharacter(g, 1, px, py, Color.BLACK, "222093013")
-      //g.drawImage(tilesetRed, px, py, null)
+    // va falloir trouver un moyen de ne repaint que ce qui nous intéresse
+
+    if (null != tilesetWhite){
+      for(i <- 1 to 80 ){
+        for(j <- 1 to 45 ){
+          paintCharacter(g, (i*17+j*31)%256, i, j, Color.BLACK, game_colors(current_color))
+        }
+      }
+    }
   }
 
   focusable = true
@@ -107,7 +127,10 @@ class GamePanel(main_frame: MainFrame ) extends Panel {
   //**************************************************************//
   this.reactions += {
     case KeyPressed(_,k,_,_) => println(k+"\n")
-    case MouseClicked(_,_,_,_,_) => {}
+    case MouseClicked(_,_,_,_,_) => 
+      current_color = (current_color+1)%4
+      this.repaint()
+      println("map size : " + tileSets.size)
   }
 }
 

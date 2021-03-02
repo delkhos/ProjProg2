16d8199ab70dd0b0f29d9cdab2776f22969e6f04
Khleone <ndumange@outlook.fr>
2021-03-02 10:16:06 +0100

    commenté

--- src/main/scala/monsters.scala
+++ src/main/scala/monsters.scala
@@ -2,6 +2,7 @@ package rogue
 
 import java.awt.{Color,Graphics2D, Graphics}
 
+// definition of the montsers, with their properties
 class Monster(arg_pos: Position, sprite: Sprite, collidable: Boolean, maxHealth: Int, ia: ArtificialIntelligence, hitChance: Int, hitDamage: Int,name_arg: String,name_color: String) extends LivingEntity(arg_pos,sprite, collidable, maxHealth, hitChance, hitDamage, name_arg, name_color) {
   val own_ia = ia
   def processDecision(game: GameObject){
@@ -9,6 +10,7 @@ class Monster(arg_pos: Position, sprite: Sprite, collidable: Boolean, maxHealth:
   }
 }
 
+// creation of some monsters
 
 class Goblin(arg_pos: Position) extends Monster(arg_pos,
   new Sprite( Array[SubSprite](new SubSprite(288,"161200060"), new SubSprite(287, "102051000")), new Color((1.0).toFloat,(1.0).toFloat,(1.0).toFloat,(0.0).toFloat) ),
@@ -27,7 +29,3 @@ class Bat(arg_pos: Position) extends Monster(arg_pos,
   true,10,new FastIA,90,2,"Bat","153000204"){}
 
 
-// pour les monstres ajouter une fonction spawn
-//
-// L'inventaire a été ajouté mais il faudrait ajouter une fonctionnalité pour pouvoir "drop" un item en utilisant le clic droit
-// Si le temps ajouter un objet de victoire. Si on le ramasse la partie est gagné (histoire d'avoir un moyen de victoire temporaire (peut être faudrait il en ramasser 4 pour plus de challenge). Et ajouter le fait que l'on perde si le héros meurt. Si temps et courage, ajouter plusieurs étages, mais pkutôt à faire pendant la seconde partie du projet.
